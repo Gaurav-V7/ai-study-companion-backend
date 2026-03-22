92953a0f2c649ab51f1a0120c00499a610443944
Gaurav-V7 <gauravvadnere789@gmail.com>
2026-03-22 20:34:07 +0530

    feat: Added Ask API for answering to the user asked questions

--- src/controllers/main.controllers.js
+++ src/controllers/main.controllers.js
@@ -1,4 +1,4 @@
-import { processImage } from "../services/main.services.js";
+import { generateAI, processImage } from "../services/main.services.js";
 import { errorResponse, messageResponse } from "../utils/common.js";
 import { getCached, saveCache } from "../utils/cache.js";
 
@@ -28,6 +28,42 @@ export class MainController {
       saveCache(file.originalname, result);
 
       return messageResponse(res, "File uploaded successfully", 200, result);
+    } catch (error) {
+      return errorResponse(res, error);
+    }
+  }
+
+  static async ask(req, res) {
+    try {
+      const { question, context } = req.body;
+
+      if (!question) {
+        return errorResponse(res, {
+          message: "Question is required",
+          statusCode: 400,
+        });
+      }
+
+      const prompt = `
+      You are a helpful study assistant.
+      
+      Use the following study material to answer the question.
+      
+      Context:
+      ${context}
+
+      Question:
+      ${question}
+
+      Guidelines:
+      - Answer clearly and simply
+      - Keep it relevant to the context
+      - If answer is not in the context, say "Not found in the provided notes"
+      `;
+
+      const result = await generateAI(prompt);
+
+      return messageResponse(res, "The answer for the question", 200, result);
     } catch (error) {
       return errorResponse(res, error);
     }
--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -6,4 +6,6 @@ const router = express.Router();
 
 router.post("/upload", upload.single("file"), MainController.upload);
 
+router.post("/ask", MainController.ask);
+
 export default router;
